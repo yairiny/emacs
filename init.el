f91466a5edd3e5fe57d0882f9e3e87054a30d4a9
Yair Iny <yair.iny@gmail.com>
2013-08-26 11:53:09 +1000

    latest

--- init.el
+++ init.el
@@ -70,7 +70,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "purple1"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "orange1"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse1"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "firebrick1"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "aquamarine1"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "SlateBlue1"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "maroon1"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "red1"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "tan1")))))
 
 ;;rainbow mode
 (require 'rainbow-delimiters)
