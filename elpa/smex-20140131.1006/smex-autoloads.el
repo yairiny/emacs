79ccd8c381b5aa19a25b9e74e6938fd9012339e5
Yair Iny <yair.iny@gmail.com>
2014-02-26 09:44:20 +1100

    after updates, before cider

---
+++ elpa/smex-20140131.1006/smex-autoloads.el
@@ -0,0 +1,33 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (21255
+;;;;;;  60871 0 0))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex "smex" "\
+
+
+\(fn)" t nil)
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (21255 60871 878085 0))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
--- init.el
+++ init.el
@@ -155,3 +155,5 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(load-file "~/.emacs.d/my-inits.el")
